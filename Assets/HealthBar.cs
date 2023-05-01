180038a22cfb61d360a8e9e5294b640154d48502
Alexandre <alxandre.rbrt@gmail.com>
2023-05-01 17:47:51 +0200

    add : display health point

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using static System.Net.Mime.MediaTypeNames;
 
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
+    //start avec la vie max
+    void Start()
+    {
+        SetMaxHealth((int)slider.maxValue);
+        SetHealth((int)slider.value);
+    }
+
+    //modificateyr vie max
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        //met les valeur dans le champs text "HealthValue"
+        transform.Find("HealthValue").GetComponent<Text>().text = health.ToString() + " / " + slider.maxValue.ToString();
     }
 
+    //modificateur vie
     public void SetHealth(int health)
     {
         slider.value = health;
+        
+        //met les valeur dans le champs text "HealthValue"
+        transform.Find("HealthValue").GetComponent<Text>().text = health.ToString() + " / " + slider.maxValue.ToString();
     }
-
 }
