1bf1f1da9516d4939e6d83ef7e3f3a8e69738dbb
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2023-05-16 18:30:33 +0200

    fixed issues hud camera, removed askname for the moment

--- Assets/Scripts/Player/Triggers.cs
+++ Assets/Scripts/Player/Triggers.cs
@@ -6,7 +6,6 @@ public class Triggers : MonoBehaviour
 {
     public GameObject player;
     public GameObject text;
-    public GameObject AskNameWindow;
 
     // with position values
     private float x;
@@ -17,20 +16,16 @@ public class Triggers : MonoBehaviour
     void Update()
     {
         
-        //if askName is active
-        if (AskNameWindow.activeSelf == false)
+        if (player.transform.position.z < 810)
         {
-            if (player.transform.position.z < 810)
-            {
-                // calls SetText() from TitleTextManagement.cs
-                text.GetComponent<TitleTextManagement>().SetText("Far Far Away");
-            }
+            // calls SetText() from TitleTextManagement.cs
+            text.GetComponent<TitleTextManagement>().SetText("Far Far Away");
+        }
 
-            if (player.transform.position.z >= 890)
-            {
-                // calls SetText() from TitleTextManagement.cs
-                text.GetComponent<TitleTextManagement>().SetText("La mine");
-            }
+        if (player.transform.position.z >= 890)
+        {
+            // calls SetText() from TitleTextManagement.cs
+            text.GetComponent<TitleTextManagement>().SetText("La mine");
         }
     }
 }
